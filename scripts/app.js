8adeb2b44d1a3e2f8c62acaa6b8f7f20d161b74f
Nathan Huff <huffn@ldschurch.org>
2015-11-16 12:05:42 -0700

    Added ability to add new secrets

---
+++ scripts/app.js
@@ -0,0 +1,28 @@
+(function(document) {
+  'use strict';
+
+  var app = document.querySelector('#app');
+  
+  app.sendMyMessage = function(evt, detail, sender) {
+    var text = app.$.input;
+    if(!text.value){
+      return false;
+    }
+    var prof = app.profileData;
+    var picture = prof.user.picture.thumbnail;
+    var username = prof.user.username;
+
+    app.$.chatlist.$.base.add({
+      message: text.value,
+      thumbnail: picture,
+      username: username
+    });
+    text.value = null;
+    
+    setTimeout(function () {
+      var chats = document.querySelectorAll('.huff-chat-list.bubble');
+      chats[chats.length-1].scrollIntoView();
+    }, 10);
+  }
+
+})(document);
